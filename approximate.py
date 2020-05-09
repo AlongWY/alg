3e70ff54598f13918bdda22839cdd86303bdb221
ylfeng <ylfeng@ir.hit.edu.cn>
2020-05-09 18:47:55 +0800

    集合覆盖问题 1. 贪心算法重写 2. 线性规划舍入法

--- approximate.py
+++ approximate.py
@@ -1,24 +1,52 @@
 def greedy(X: set, F: list):
-    U = X
+    price = {x: 1 for x in X}
+    U = set()
     C = []
-    while len(U):
-        max_f = None
-        max_common = 0
-        for i, f in enumerate(F):
-            common = len(f & U)
-            if common > max_common:
-                max_f = i
-                max_common = common
-        U = U - F[max_f]
-        C.append(F[max_f])
-        F.pop(max_f)
+    while U != X:
+        s_u = None
+        min_s = None
+        min_cost = float("inf")
+        for i, s in enumerate(F):
+            temp = s - U
+            cost = (1 / len(temp)) if len(temp) > 0 else float("inf")
+            if cost < min_cost:
+                min_s = i
+                s_u = temp
+                min_cost = cost
+
+        for e in s_u:
+            price[e] = min_cost
+
+        U.update(s_u)
+        C.append(F.pop(min_s))
 
     return C
 
 
+def linear(X: set, F: list):
+    from itertools import chain
+    from collections import Counter
+    from pulp import LpProblem, LpMinimize, LpVariable, lpSum, value
+    problem = LpProblem(sense=LpMinimize)
+    variables = [LpVariable(f"x{i}", 0) for i in range(len(F))]
+
+    # 最小化函数
+    problem += lpSum(variables)
+
+    # 约束，X中的元素都至少有一个
+    for e in X:
+        problem += (lpSum([xs for idx, xs in enumerate(variables) if e in F[idx]]) >= 1)
+
+    problem.solve()
+
+    f = 1. / Counter(chain(*F)).most_common(1)[0][1]
+    return [F[index] for index, var in enumerate(variables) if value(var) > f]
+
+
 def main():
-    X = set([1, 2, 3, 4, 5, 6, 7])
-    F = [set([1, 2, 3, 4]), set([2, 3, 4]), set([5, 6, 7])]
+    X = {1, 2, 3, 4, 5, 6, 7}
+    F = [{1, 2, 3, 4}, {2, 3, 4}, {4, 5, 6, 7}]
+    print(linear(X, F))
     print(greedy(X, F))
 
 
